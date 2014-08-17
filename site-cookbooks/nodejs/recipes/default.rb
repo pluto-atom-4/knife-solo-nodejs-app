b3235cbd012500f37560f0ecd20100e24c200328
kaede <kaede-git-2013@example.local>
2014-08-17 07:21:19 -0700

    Added configureing httpd  proxies

--- site-cookbooks/nodejs/recipes/default.rb
+++ site-cookbooks/nodejs/recipes/default.rb
@@ -13,3 +13,23 @@ end
 package "npm" do
   action :install
 end
+
+package "httpd" do
+  action :install
+end
+
+git "/var/www/nodejs-project" do
+   repository "https://github.com/pluto-atom-4/nodejs-project"
+   reference "master"
+   action :checkout
+   user "root"
+   group "root"
+end
+
+template "10_nodejs.conf" do
+  path   "/etc/httpd/conf.d/10_nodejs.conf"
+  source "etc/httpd/conf.d/10_nodejs.conf.erb"
+  owner  "root"
+  group  "root"
+  mode 0644
+end
